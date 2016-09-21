b2f8bf2443e64cc2fc5d46fa7584f6780ed4f984
Admin <git@webgk.ru>
2016-09-21 14:59:47 +0300

    CP-4374 create include area on index page

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("WebGK - создание интернет-магазинов, создание сайтов, продвижение интернет-магазинов, продвижение сайтов");
-?>
-
-
-
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
